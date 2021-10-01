db45b0327f39fcc820724fb2853a3a31a6d97cf8
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2021-09-30 23:33:09 -0600

    commit tree 1c3fd6ca3188600b5bb6bf16da646455e2cf43e3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :students
+  resources :students, except: [:index, :destroy]
+  resources :school_classes, except: [:index, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
