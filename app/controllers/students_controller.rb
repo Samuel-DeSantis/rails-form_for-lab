e52ab8c71ba869e2a9e5699816b0bd2528a17ad7
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2021-09-30 23:33:19 -0600

    complete

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,14 +1,11 @@
 class StudentsController < ApplicationController
 
-    def index
-        @students = Student.all
-    end
-
     def show
         @student = Student.find(params[:id])
     end
 
     def new
+        @student = Student.new
     end
 
     def create
@@ -22,6 +19,7 @@ class StudentsController < ApplicationController
     def update
         @student = Student.find(params[:id])
         @student.update(student_params(:first_name, :last_name))
+        redirect_to student_path(@student)
     end
 
     private
